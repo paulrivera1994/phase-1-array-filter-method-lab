76a98d7cf781c9ad3e5a62249c5ce47c87d56c81
Paul Rivera <juniorrivera81@yahoo.com>
2022-05-25 15:08:58 -0600

    commit tree 29bfdbd84c8f912ff28ab8d19641eaf299d04ab6

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(array, string){
+    return array.filter(
+        (match) => match.toLowerCase() === string.toLowerCase()
+    );
+}
+function fuzzyMatch(array, string){
+    return array.filter(
+        (match) => 
+            match.toLowerCase().indexOf(string.toLowerCase()) === 0
+    );
+}
+function matchName(array, string){
+    return array.filter((record) => record.name === string);
+    
+}
